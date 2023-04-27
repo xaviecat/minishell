7eefce63288642c854a2a00faf6f623fd5a7b9df
arhkaz <sylvain.arhkaz@gmail.com>
2023-04-27 11:03:16 +0200

    changed the makefile, add the flag for readline and code little fx minishell

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -3,18 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: syluiset <syluiset@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:39:41 by xcharra           #+#    #+#             */
-/*   Updated: 2023/04/24 11:59:15 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/04/26 14:17:33 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../incs/minishell.h"
 
-int	main(void)
+
+void	minishell(char **envp)
 {
-	printf("salut minihell !\n");
+	char	*line;
 
-	return (0);
+	(void)envp;
+	while (1)
+	{
+		line = readline("$ ");
+		printf("%s\n", line);
+		if (ft_strncmp(line, "exit", 4) == 0)
+			break;
+	}
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	(void)argv;
+	if (argc == 1)
+		minishell(envp);
+	return (1);
 }
