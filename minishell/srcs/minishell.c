5d628d9286e2ef771d7219a340dfb3a2387912e2
arhkaz <sylvain.arhkaz@gmail.com>
2023-05-03 12:22:42 +0200

    add pwd test

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: syluiset <syluiset@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:39:41 by xcharra           #+#    #+#             */
-/*   Updated: 2023/04/27 16:45:08 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/05/03 12:04:18 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@ void	minishell(char **envp)
 	char	*line;
 	char 	**arg;
 	(void)envp;
-
+	char	path[256];
 	while (1)
 	{
 		line = readline("$ >> ");
 		if (ft_strncmp(line, "exit", 5) == 0)
 			break ;
+		if (ft_strncmp(line, "pwd", 4) == 0)
+		{
+			getcwd(path, sizeof(path));
+			printf("%s\n", path);
+		}
 		if (line && *line)
 			add_history(line);
 		arg = parsing_argu(line);
