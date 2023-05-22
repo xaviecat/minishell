6aae6a91edfa69d4da896c2a7abc453c8ea0cf14
xcharra <xcharra@student.42lyon.fr>
2023-05-22 14:28:36 +0200

    is forbidden char in unhandled char to iterate the char liste to fund any unhandled char (&, &&, ||, ;)

--- minishell/srcs/parsing/unhandled_char.c
+++ minishell/srcs/parsing/unhandled_char.c
@@ -6,51 +6,38 @@
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 17:43:08 by xcharra           #+#    #+#             */
-/*   Updated: 2023/05/17 18:41:56 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/05/22 14:25:36 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
 
+/**
+ * @brief Iterate through the list to see if there are any unhandled characters
+ * @param lst list of characters
+ * @return true if there are any unhandled characters
+ * @return false if it's not the case
+ */
 bool	is_forbidden_char(t_char_lst *lst)
 {
 	while (lst)
 	{
-		if (lst->c == '&' && lst->next && lst->next->c == '&'
-			&& !lst->s_quote && !lst->d_quote)
-			return (true), ft_fdprintf(2, "minishell: syntax error near unexpected token '&&'\n");
+		if (lst->c == '&' && !lst->s_quote && !lst->d_quote)
+		{
+			if (lst->next && lst->next->c == '&')
+				return (ft_fdprintf(2,
+						"minishell: feature not handled: '&&'\n"), true);
+			return (ft_fdprintf(2,
+					"minishell: feature not handled: '&'\n"), true);
+		}
 		if (lst->c == '|' && lst->next && lst->next->c == '|'
 			&& !lst->s_quote && !lst->d_quote)
-			return (true), ft_fdprintf(2, "minishell: syntax error near unexpected token '||'\n");
+			return (ft_fdprintf(2,
+					"minishell: feature not handled: '||'\n"), true);
 		if (lst->c == ';' && !lst->s_quote && !lst->d_quote)
-			return (true), ft_fdprintf(2, "minishell: syntax error near unexpected token ';'\n");
+			return (ft_fdprintf(2,
+					"minishell: feature not handled: ';'\n"), true);
 		lst = lst->next;
 	}
 	return (false);
 }
-
-/**
- * @brief make sure that the command we got as not a character we don't want
- * @param command
- * @return 1 if command is good, 0 if is not
- *//*
-
-int	check_command_is_fine(char *command)
-{
-	int	i;
-
-	i = 0;
-	while (command[i])
-	{
-		if (command[i] == '\\')
-			return (0); // ! ERROR
-		if (command[i] == ';')
-			return (0); // ! ERROR
-		if (ft_strncmp(command + i, "&&", 2) == 0)
-			return (0); // ! ERROR
-		if (ft_strncmp(command + i, "||", 2) == 0)
-			return (0); // ! ERROR
-		i++;
-	}
-	return (1);
-}*/
