d441012224bef3ea6f147c9297d4e79ec20239d2
xcharra <xcharra@student.42lyon.fr>
2023-06-01 18:55:04 +0200

    start to fixsigsev error on harmonize_spaces.c

--- minishell/srcs/parsing/harmonize_spaces.c
+++ minishell/srcs/parsing/harmonize_spaces.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hormonize_spaces.c                                 :+:      :+:    :+:   */
+/*   harmonize_spaces.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:20:59 by xcharra           #+#    #+#             */
-/*   Updated: 2023/05/31 11:11:04 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/06/01 17:53:42 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,16 @@ void	remove_extra_spaces(t_char_lst *lst, t_garbage **gb)
 	{
 		while (lst && (lst->s_quote || lst->d_quote))
 			lst = lst->next;
-		while (lst && lst->c == ' ' && lst->next->c == ' ')
+		while (lst && lst->next && lst->c == ' ' && lst->next->c == ' ')
 			char_lst_delone(&lst, gb);
 		if (lst)
 			lst = lst->next;
 	}
 }
 
-void	add_some_spaces(t_char_lst *lst, t_garbage **gb)
+void	add_some_space_near_pipes(t_char_lst *lst, t_garbage **gb)
 {
+
 	while (lst)
 	{
 		while (lst && (lst->s_quote || lst->d_quote))
@@ -62,27 +63,33 @@ void	add_some_spaces(t_char_lst *lst, t_garbage **gb)
 	}
 }
 
+void	add_some_space_near_a_brackets(t_char_lst *lst, t_garbage **gb, char c)
+{
+	while (lst)
+	{
+		while (lst && (lst->s_quote || lst->d_quote))
+			lst = lst->next;
+		if (lst && lst->prev && lst->c == c && lst->prev->c != c && lst->prev->c != ' ')
+			char_lst_add_in(&lst, prev, char_lst_new(' ', gb));
+		if (lst && lst->next && lst->c == c && lst->next->c != c && lst->next->c != ' ')
+			char_lst_add_in(&lst, next, char_lst_new(' ', gb));
+		if (lst)
+			lst = lst->next;
+	}
+}
+
 void	harmonize_spaces(t_char_lst *lst, t_garbage **gb)
 {
 	remove_extra_spaces(lst, gb);
-	add_some_spaces(lst, gb);
+	add_some_space_near_pipes(lst, gb);
+	add_some_space_near_a_brackets(lst, gb, '<');
+	add_some_space_near_a_brackets(lst, gb, '>');
 }
 
 /*
  *
- * tant que je ne suis pas entre quotes
- 	*
-*
- *
- *
- *
  * salut| cat
  * salut|
- *
- *
- *
- *
- *
  * echo hello world > salut| cat salut    |   echo "                  salut                   "
  * echo hello world > salut| cat -en |cat salut|echo "|sal|ut| |"
  */
