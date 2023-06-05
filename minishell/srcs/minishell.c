1ba2a8d12a635df95b6b99fb485b9a88a626c52a
xcharra <xcharra@student.42lyon.fr>
2023-06-05 16:10:30 +0200

    norm on harmonize spaces

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:39:41 by xcharra           #+#    #+#             */
-/*   Updated: 2023/06/05 13:29:15 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/06/05 15:58:44 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,7 @@ void	minishell(char **envp)
 			add_history(line);
 		minish = create_minishell(envp, envp_sh);
 		if (!minish)
-			return ; // ! ERROR
+			return ; // ! ERROR free line
 		if (!(create_char_lst_with_c_inside(line, &minish)))
 		{
 			free(line);
