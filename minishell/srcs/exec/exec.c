57b95eee9b4b841dddd71d83162f5e5740b7e6bd
xcharra <xcharra@student.42lyon.fr>
2023-06-14 18:38:15 +0200

    fix exec.c

--- minishell/srcs/exec/exec.c
+++ minishell/srcs/exec/exec.c
@@ -6,20 +6,19 @@
 /*   By: xcharra <xcharra@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 19:25:52 by syluiset          #+#    #+#             */
-/*   Updated: 2023/06/14 16:21:31 by xcharra          ###   ########.fr       */
+/*   Updated: 2023/06/14 18:36:14 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
 
-void	exec_all(t_minish *msh)
 /**
  * @brief Execute all function pars in the cmd_list, it should be an builtin or
  * a command with complete path
  * @param minish
  * @return 0 if all worked fine, 1 if an error occurred
  */
-int	exec_all(t_minish *minish)
+int	exec_all(t_minish *msh)
 {
 	while (msh->cmds)
 	{
