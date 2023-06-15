43b175d2d1dafe8448e911de2a62d74b8384d7d5
xcharra <xcharra@student.42lyon.fr>
2023-06-15 10:59:36 +0200

    change .h

--- minishell/incs/minishell.h
+++ minishell/incs/minishell.h
@@ -14,181 +14,42 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/incs/libft.h"
+
 # include "colors.h"
 # include "error_msgs.h"
+# include "../libft/incs/libft.h"
 
 /* malloc, free, exit, getenv, tcsetattr, tcgetattr, */
 # include <stdlib.h>
-
 /* open */
 # include <fcntl.h>
-
 /* close, read, write, access, dup, dup2, execve, fork, pipe, unlink
 , rl_*, getcwd, chdir, stat, lstat, fstat, isatty, ttyname,ttyslot */
 # include <unistd.h>
 # include <sys/stat.h>
-
 /* perror printf readline ??*/
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-
 /* strerror */
 # include <string.h>
-
 /* wait, waitpid, wait3, wait4 */
 # include <sys/wait.h>
-
 /* signal, sigaction, sigemptyset, siggadset, kill */
 # include <signal.h>
-
 /* opendir,readdir, closedir */
 # include <dirent.h>
-
 /* ioctl,  */
 # include <sys/ioctl.h>
-
 /* tgetent, tgetflag, tgetnum, tgetstrr, tgoto, tputs */
 # include <curses.h>
 # include <term.h>
-
 /* errno */
 # include <errno.h>
 
+# include "structures.h"
+
 extern int	g_exit_status;
-
-/* structure */
-
-	/* enum */
-
-typedef enum e_position
-{
-	prev,
-	next,
-}	t_position;
-
-typedef enum e_type_char
-{
-	space,
-	s_quote,
-	d_quote,
-	charc,
-	c_pipe,
-	dash,
-	a_bracket,
-	dollar
-}			t_type_char;
-
-typedef enum e_type_word
-{
-	not_define,
-	command,
-	builtin,
-	param,
-	redir,
-	hd,
-	appnd,
-	w_pipe,
-	expand,
-	open_file,
-	delimiteur,
-	infile,
-	outfile,
-	in_d_quote,
-	in_s_quote
-}			t_type_word;
-
-typedef enum e_type_redir
-{
-	in,
-	inin,
-	out,
-	outout
-}			t_type_redir;
-
-	/* list chaine*/
-
-typedef struct s_garbage_list
-{
-	void					*content;
-	struct s_garbage_list	*next;
-	struct s_garbage_list	*prev;
-}				t_garbage_list;
-
-typedef struct s_garbage
-{
-	t_garbage_list	*first;
-	t_garbage_list	*last;
-}				t_garbage;
-
-typedef struct s_redir_list
-{
-	t_type_redir		redir;
-	char				*filename;
-	struct s_redir_list	*next;
-	struct s_redir_list	*last_added;
-}				t_redir_list;
-
-typedef struct s_fd_list
-{
-	int					in;
-	int					out;
-	struct s_fd_list	*next;
-	struct s_fd_list	*last_added;
-}				t_fd_list;
-
-typedef struct s_w_cmd_list
-{
-	char				*cmd;
-	bool				s_quote;
-	bool				d_quote;
-	struct s_w_cmd_list	*next;
-	struct s_w_cmd_list	*last_added;
-}				t_w_cmd_list;
-
-typedef struct s_cmd_list
-{
-	t_w_cmd_list		*cmd;
-	char				*cmdpath; // strjoin PATH+CMD
-	char				**cmdtab;
-	bool				builtin;
-	struct s_redir_list	*redirs;
-	struct s_fd_list	*fds;
-	struct s_cmd_list	*next;
-	struct s_cmd_list	*previous;
-	struct s_cmd_list	*last_added;
-}				t_cmd_list;
-
-typedef struct s_word_lst
-{
-	char				*word;
-	t_type_word			type;
-	struct s_word_lst	*next;
-	struct s_word_lst	*prev;
-	struct s_word_lst	*last_added;
-}						t_word_lst;
-
-typedef struct s_char_lst
-{
-	int					pipe;
-	char				c;
-	t_type_char			type;
-	bool				s_quote;
-	bool				d_quote;
-	struct s_char_lst	*prev;
-	struct s_char_lst	*next;
-	struct s_char_lst	*last_added;
-}				t_char_lst;
-
-typedef struct s_minish
-{
-	char		**envp;
-	t_cmd_list	*cmds;
-	t_char_lst	*lst_c;
-	t_word_lst	*lst_w;
-	t_garbage	*garbage;
-}				t_minish;
 
 typedef bool	(*t_unhandled_char)(t_char_lst *);
 
--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -117,7 +117,7 @@ void	minishell(char **envp)
 			continue ;
 		}
 		harmonize_spaces(&(minish->lst_c), &(minish->garbage));
-		print_lst_char(minish->lst_c);
+//		print_lst_char(minish->lst_c);
 		if (!(create_word_lst(&minish)))
 			free_and_exit_minish(minish, &envp_sh);
 		if ((!check_pipe_and_redir(&(minish->garbage), &(minish->lst_w))))
@@ -126,7 +126,7 @@ void	minishell(char **envp)
 		if (!expand_commands(minish))
 			return (ft_free_all(&(minish->garbage)),
 				free_and_exit_minish(minish, &envp_sh), (void) 0);
-		print_lst_word(minish->lst_w);
+//		print_lst_word(minish->lst_w);
 		if (!(sh_pars(&minish)))
 			free_and_exit_minish(minish, &envp_sh);
 		if (!ft_del_quotes(minish))
