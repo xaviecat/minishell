19f8cae5645fa4045fdf60bd459bb792a84d554f
arhkaz <sylvain.arhkaz@gmail.com>
2023-06-20 13:08:55 +0200

    Adding changes on b_exit

--- minishell/incs/minishell.h
+++ minishell/incs/minishell.h
@@ -111,7 +111,7 @@ void			free_error_word_lst(t_garbage **gb, t_word_lst **lst);
 int				b_pwd(t_msh *msh);
 int				b_cd(t_msh *msh);
 int				b_echo(t_msh *msh);
-void			b_exit(t_msh *msh);
+int				b_exit(t_msh *msh);
 int				b_export(t_msh *msh, t_cmd_lst *cmds);
 int				b_env(t_msh *msh);
 int				b_unset(t_msh *sh);
