90eac8c07d940248e245dd0081c8730878ead7be
arhkaz <sylvain.arhkaz@gmail.com>
2023-06-28 19:10:07 +0200

    changes echo and try test from google docs

--- minishell/incs/minishell.h
+++ minishell/incs/minishell.h
@@ -219,6 +219,7 @@ bool			is_backslash_error(t_char_lst *lst);
 bool			is_dollar_error(t_char_lst *lst);
 bool			is_exclamation_error(t_char_lst *lst);
 bool			is_colon_error(t_char_lst *lst);
+bool			is_parenthese_error(t_char_lst *lst);
 
 /* heredoc */
 int				heredoc_handling(t_msh *msh);
