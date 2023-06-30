af0cdda0a703c0cf5b76a6b1fdf647c54bdac698
ArhKaZ <59173332+ArhKaZ@users.noreply.github.com>
2023-06-30 12:18:26 +0200

    Revert "Merge pull request #141 from xaviecat/syluiset"
    This reverts commit 39d853ab5394a827a843acb06d6cb7a930cc783b.

--- minishell/incs/minishell.h
+++ minishell/incs/minishell.h
@@ -221,7 +221,6 @@ bool			is_exclamation_error(t_char_lst *lst);
 bool			is_colon_error(t_char_lst *lst);
 bool			is_parenthese_error(t_char_lst *lst);
 bool			is_star_error(t_char_lst *lst);
-bool			is_dot_error(t_char_lst *lst);
 
 /* heredoc */
 int				heredoc_handling(t_msh *msh);
--- minishell/srcs/parsing/is_error_fct2.c
+++ minishell/srcs/parsing/is_error_fct2.c
@@ -6,7 +6,7 @@
 /*   By: syluiset <syluiset@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 12:19:56 by xcharra           #+#    #+#             */
-/*   Updated: 2023/06/30 11:47:43 by syluiset         ###   ########.fr       */
+/*   Updated: 2023/06/28 13:13:35 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,5 @@ bool	is_parenthese_error(t_char_lst *lst)
 		return (ft_fdprintf(2, RED MSH P_OPEN RESET), true);
 	else if (lst->c == ')' && !lst->s_quote && !lst->d_quote)
 		return (ft_fdprintf(2, RED MSH P_CLOSE RESET), true);
-	return (false);
-}
-
-bool	is_dot_error(t_char_lst *lst)
-{
-	if (lst->c == '.' && !lst->s_quote && !lst->d_quote && !lst->next && !lst->prev)
-		return (ft_fdprintf(2, RED MSH DOT RESET), true);
 	return (false);
 }
