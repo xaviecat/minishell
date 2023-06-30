160b8c137dd11808e8712ba625108536d4c3b7fd
xcharra <xcharra@student.42lyon.fr>
2023-06-30 17:27:52 +0200

    fix . .. '' cmd

--- minishell/srcs/parsing/is_error_fct2.c
+++ minishell/srcs/parsing/is_error_fct2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_error_fct2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: syluiset <syluiset@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 12:19:56 by xcharra           #+#    #+#             */
-/*   Updated: 2023/06/28 13:13:35 by syluiset         ###   ########.fr       */
+/*   Updated: 2023/06/30 17:00:44 by xcharra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,8 @@ bool	is_parenthese_error(t_char_lst *lst)
 
 bool	is_dot_error(t_char_lst *lst)
 {
-	if (lst->c == '.' && !lst->s_quote && !lst->d_quote && !lst->next && !lst->prev)
+	if (lst->c == '.' && !lst->s_quote
+		&& !lst->d_quote && !lst->next && !lst->prev)
 		return (ft_fdprintf(2, RED MSH DOT RESET), true);
 	return (false);
 }
